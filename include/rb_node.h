534de30f8547ced77c882215866584176613aefc
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-04-11 13:00:09 +0530

    include: rb_node.h: declare Color, RBNode
    Declare the enum Color for representing if the color of a node is Red or
    Black, and declare the class RBNode which will represent a single node
    in the Red Black Tree.
    
    TODO:
    	* Define constructors, and destructors (as/if necessary)
    	* Define setters and getters?
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- include/rb_node.h
+++ include/rb_node.h
@@ -1,4 +1,21 @@
 #ifndef RB_NODE_H
 #define RB_NODE_H
 
+enum Color { Red, Black };
+
+class RBNode
+{
+	private:
+		int value_;
+		RBNode *parent_;
+		RBNode *left_;
+		RBNode *right_;
+		Color color_;
+	public:
+	/*
+	 * 1. Define ctor.
+	 * 2. Define setters and getters too? (so code looks a bit cleaner)
+	 */		
+};
+
 #endif
