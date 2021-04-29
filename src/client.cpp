ea479b3ef15f9826fb0c9c6506edf940401db4d8
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-04-29 06:27:10 +0530

    client.cpp: set up tree from RBT example in CLRS
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/client.cpp
+++ src/client.cpp
@@ -6,31 +6,51 @@ using namespace std;
 int main(void)
 {
 	RBTree<int> *tree = new RBTree<int>();
-	RBNode<int> *rootnode = new RBNode<int>(1);
-	RBNode<int> *l = new RBNode<int>(2, nullptr, nullptr, nullptr, BLACK);
-	RBNode<int> *r = new RBNode<int>(3);
-	RBNode<int> *ll = new RBNode<int>(4);
-	RBNode<int> *lr = new RBNode<int>(5);
-	RBNode<int> *rl = new RBNode<int>(6);
-	RBNode<int> *rr = new RBNode<int>(7);
+
+	// same example as CLRS
+	RBNode<int> *lll = new RBNode<int>(2);
+	RBNode<int> *lr = new RBNode<int>(6);
+	RBNode<int> *rl = new RBNode<int>(9);
+	RBNode<int> *rrll = new RBNode<int>(12);
+	RBNode<int> *rrlr = new RBNode<int>(17);
+	RBNode<int> *rrrrl = new RBNode<int>(20);
+
+	RBNode<int> *rrrr = new RBNode<int>(22, nullptr, rrrrl);
+
+
+	RBNode<int> *rrl = new RBNode<int>(14, nullptr, rrll, rrlr);
+	RBNode<int> *rrr = new RBNode<int>(19, nullptr, nullptr, rrrr);
+
+	RBNode<int> *ll = new RBNode<int>(3, nullptr, lll, nullptr);
+	RBNode<int> *rr = new RBNode<int>(18, nullptr, rrl, rrr);
+
+	RBNode<int> *l = new RBNode<int>(4, nullptr, ll, lr);
+	RBNode<int> *r = new RBNode<int>(11, nullptr, rl, rr);
+
+	RBNode<int> *rootnode = new RBNode<int>(7, nullptr, l, r);
+
+	l->parent_ = rootnode;
+	r->parent_ = rootnode;
+
+	ll->parent_ = l;
+	rr->parent_ = r;
+
+	rrl->parent_ = rr;
+	rrr->parent_ = rr;
+
+	rrrr->parent_ = rrr;
+	lll->parent_ = ll;
+	lr->parent_ = l;
+	rl->parent_ = r;
+	rrll->parent_ = rrl;
+	rrlr->parent_ = rrl;
+	rrrrl->parent_ = rrrr;
 
 	tree->root_ = rootnode;
-	rootnode->left_ = l;
-	rootnode->right_ = r;
-	l->left_ = ll;
-	l->right_ = lr;
-	r->left_ = rl;
-	r->right_ = rr;
-	ll->left_ = RBTree<int>::NIL;
-	lr->left_ = RBTree<int>::NIL;
-	rl->left_ = RBTree<int>::NIL;
-	rr->left_ = RBTree<int>::NIL;
 
-	ll->right_ = RBTree<int>::NIL;
-	lr->right_ = RBTree<int>::NIL;
-	rl->right_ = RBTree<int>::NIL;
-	rr->right_ = RBTree<int>::NIL;
 	cout << *tree;
-
+	tree->rotate_left(r);
+	cout <<"\n";
+	cout << *tree;
 	return 0;
 }
