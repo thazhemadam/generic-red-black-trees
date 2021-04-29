b754a650e1110600079ead0ca9aa3304b38dcca7
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-04-30 02:33:31 +0530

    utils.h: fix preorder_successor()
    Rewrite preorder_successor() from scratch, so it is more easily readable,
    and doesn't throw exceptions or cause segfaults.
    
    If a preorder_successor() exists, then a pointer to it is returned.
    Else, a nullptr is returned.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- include/utils.h
+++ include/utils.h
@@ -61,18 +61,36 @@ void postorder(RBNode<T> *root)
 template<typename T>
 RBNode<T>* preorder_successor(RBNode<T> *node)
 {
-	if (node->left_)
+	if(node == nullptr)
+		return nullptr;
+
+	if (node->left_)		// has left child.
 		return node->left_;
 
-	RBNode<T> *curr = node, *parent = curr->parent_;
-	while (parent != nullptr && parent->right_ == curr) {
-		curr = curr->parent_;
-		parent = parent->parent_;
-	}
-	if (parent == NULL)
-		return NULL;
+	else if (node->right_)		// has right child.
+		return node->right_;
 
-	return parent->right_;
+	if(node->parent_ == nullptr)	// has no children, and is root.
+		return nullptr;
+
+	RBNode<T> *parent = node->parent_;
+
+	if(node == parent->left_ && parent->right_ != nullptr)	// leaf node, which has a right sibling.
+		return parent->right_;
+
+	// leaf node. has an ancestor, which is a left child, with a right sibling
+	RBNode<T> *ancestor = node->parent_;
+	parent = ancestor->parent_;		// parent is the parent of the ancestor
+	while ((parent != nullptr) && (parent->left_ != ancestor) && (parent->right_ == nullptr)) {
+		ancestor = parent;
+		parent = ancestor->parent_;
+	}
+
+	if(ancestor == parent->left_)
+		return parent -> right_;
+
+	// undefined case.
+	return nullptr;
 }
 
 #endif
