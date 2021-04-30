thazhemadam/generic-red-black-trees

commit fa3c1db845d0a0b9955f00c09f6f8cc15518a8ea

copy constructor set for the tree

include/rb_tree.h
@@ -19,8 +19,7 @@ public:
 // special functions
 	// constructor
 	RBTree();
-	RBTree(const RBTree<T>* & rhs);
-	RBTree(const RBTree<T> &rhs);
+	RBTree(const RBTree<T> *rhs);
 
 	RBNode<T> *root_;		// to be made private!
 	static RBNode<T> *NIL;		// to made private!
@@ -68,32 +67,30 @@ RBTree<T>::RBTree()
 }
 
 // copy ctor
+// copy ctor
 template<typename T>
-RBTree<T>::RBTree(const RBTree<T> &rhs)
-:root_(new RBTree<T>())
+RBTree<T>::RBTree(const RBTree<T> *rhs)
 {
-	cout << "Yo";
-	//copy_tree(root_,rhs.root_);
+	cout << "Bruh y u give me bt\n";
+	clone(rhs,this);
 }
 
 template<typename T>
-void clone(RBTree<T>*one,RBTree<T>*two)
+void clone(const RBTree<T>*&one,RBTree<T>*two)
 {
 	two->root_ = new RBNode<T>(one->root_->value_);
-	cout << two->root_->value_ <<"  1\n";
+	//cout << two->root_->value_ <<"  1\n";
 	copy_tree(one->root_,two->root_);
-	//cout << "5\n";
 	
 }
 
 template<typename T>
-void copy_tree(RBNode<T>* &one,RBNode<T>* &two)
+void copy_tree(RBNode<T>* const &one,RBNode<T>* &two)
 {
 	two->value_= one->value_;
-	cout << "2\n";
+
 	if(one->left_)
 	{
-		cout << "3\n";
 		RBNode<T> * temp = new RBNode<T>(one->left_->value_);
 		temp->parent_=two;
 		two->left_=temp;
@@ -101,7 +98,6 @@ void copy_tree(RBNode<T>* &one,RBNode<T>* &two)
 	}
 	if(one->right_)
 	{
-		cout << "4\n";
 		RBNode<T> * temp = new RBNode<T>(one->right_->value_);
 		temp->parent_=two;
 		two->right_=temp;

src/client.cpp
@@ -55,9 +55,12 @@ int main(void)
 	
 	cout << "---- cloning -------\n";
 
-	RBTree<int> *tree1 = new RBTree<int>();
-	clone(tree,tree1); // clone function modified
-	cout << *tree1;
+	
+	RBTree<int> tree1(tree); // tree1 is an object
+	
+	
+	//clone(tree,tree1); // clone function modified
+	cout << tree1;
 
 	return 0;
 }

commit b55f0b0dd3853ac0ba8dbbc3ce35dbef807bb9c8

rb_tree.h: fix copy constructors, and re-write helper functions
* Fix RBTree's copy constructor's prototype.
* Re-write the copy constructor and the helper functions it uses.
  Simplify the logic that was previously used, and reduce the number of
  extra function calls made.
  This can hopefully be used in the assignment operator as well.

Fixes: fa3c1db ("copy constructor set for the tree")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_node.h
@@ -26,7 +26,7 @@ public:
 		os << (node.color_ == 0 ? "black" : "red") <<"\n";
 		os << node.parent_ << "\t";
 		os << node.left_ << "\t";
-		os << node.right_ << "\t";
+		os << node.right_ << "\n";
 		return os;
 	}
 

include/rb_tree.h
@@ -19,16 +19,11 @@ public:
 	// constructor
 	RBTree();	// empty tree
 	RBTree(RBNode<T> node);	// tree with a root node
-	RBTree(const RBTree<T> *rhs);
-
+	RBTree(const RBTree<T> &rhs);
+	RBNode<T> *copy_tree(RBNode<T> *t);
 	// operator function
 	template<typename O>
 	friend ostream& operator<<(ostream& os, const RBTree<O>& tree);
-
-
-// TEMPORARY FUNCTIONS!
- 	//void copy_tree(RBNode<T>*one,RBNode<T>*two);
- 	//void clone(RBTree<T>*one,RBTree<T>*two);
 
 // Iterator class
 	class Iterator;
@@ -87,42 +82,40 @@ RBTree<T>::RBTree(RBNode<T> node)
 {}
 
 // copy ctor
-// copy ctor
 template<typename T>
-RBTree<T>::RBTree(const RBTree<T> *rhs)
+RBTree<T>::RBTree(const RBTree<T> &rhs)
+: tree_size_(rhs.tree_size_)
 {
-	cout << "Bruh y u give me bt\n";
-	clone(rhs,this);
+	if(rhs.root_ == nullptr) {
+		root_ = nullptr;
+		return;
+	}
+
+	root_ = copy_tree((rhs.root_));
 }
 
 template<typename T>
-void clone(const RBTree<T>*&one,RBTree<T>*two)
+RBNode<T>* RBTree<T>::copy_tree(RBNode<T> *root)
 {
-	two->root_ = new RBNode<T>(one->root_->value_);
-	//cout << two->root_->value_ <<"  1\n";
-	copy_tree(one->root_,two->root_);
-	
-}
+	RBNode<T> *new_left, *new_right, *new_node;
+	if(root == nullptr)
+		return nullptr;
 
-template<typename T>
-void copy_tree(RBNode<T>* const &one,RBNode<T>* &two)
-{
-	two->value_= one->value_;
+	new_left = copy_tree(root->left_);
+	new_right = copy_tree(root->right_);
 
-	if(one->left_)
-	{
-		RBNode<T> * temp = new RBNode<T>(one->left_->value_);
-		temp->parent_=two;
-		two->left_=temp;
-		copy_tree(one->left_,two->left_);
-	}
-	if(one->right_)
-	{
-		RBNode<T> * temp = new RBNode<T>(one->right_->value_);
-		temp->parent_=two;
-		two->right_=temp;
-		copy_tree(one->right_,two->right_);
-	}
+	new_node = new RBNode<int>(*root);
+	new_node -> left_ = new_left;
+	new_node-> right_ = new_right;
+
+	if(new_left != nullptr)
+		new_left -> parent_ = new_node;
+
+
+	if(new_right != nullptr)
+		new_right -> parent_ = new_node;
+		
+	return new_node;
 }
 
 // operations on trees

src/client.cpp
@@ -74,26 +74,30 @@ int main(void)
 		RBTree<int>::Iterator it1 = tree->root_;
 		RBTree<int>::Iterator it2 = tree->root_;
 
-		cout << *tree;
-		cout << "Levelorder successors!\n";
-		cout << it_lr;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, lr))) << "\n\n";
-		cout << it_l;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, l))) << "\n\n";
-		cout << it_roots;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, roots))) << "\n\n";
-		cout << it_ll;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, ll))) << "\n\n";
-		cout << it_lrl;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, lrl))) << "\n\n";
-		cout << it_lrr;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, lrr))) << "\n\n";
-		cout << it_r;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, r))) << "\n\n";
-		cout << it_rr;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, rr))) << "\n\n";
-		cout << it_new;
-		cout << (RBTree<int>::Iterator(levelorder_successor(lr, newNode))) << "\n\n";
+		// cout << *tree;
+		// cout << "Levelorder successors!\n";
+		// cout << *it_lr;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, lr))) << "\n\n";
+		// cout << it_l;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, l))) << "\n\n";
+		// cout << it_roots;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, roots))) << "\n\n";
+		// cout << it_ll;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, ll))) << "\n\n";
+		// cout << it_lrl;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, lrl))) << "\n\n";
+		// cout << it_lrr;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, lrr))) << "\n\n";
+		// cout << it_r;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, r))) << "\n\n";
+		// cout << it_rr;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, rr))) << "\n\n";
+		// cout << it_new;
+		// cout << (RBTree<int>::Iterator(levelorder_successor(lr, newNode))) << "\n\n";
+		cout << *tree <<"\n";
+		RBTree<int> newTree = RBTree<int>(*tree);
+		cout << "\n"<< newTree;
+
 	#endif
 
 