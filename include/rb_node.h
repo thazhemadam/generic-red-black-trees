thazhemadam/generic-red-black-trees

commit 29c4c568639e9b9c4bd6e552260ba1aa817c4c75

RBTree: define the basic insert() function
* Define and set up the basic insert() function that will insert the
RBNode passed as argument into the RBTree.

* Add the example case from the CLRS section on insertion as well to
show how it works correctly.

Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_tree.h
@@ -37,10 +37,10 @@ public:
 
 // operations on tree
 #ifdef RBT_UNIQUE	//TODO
-	pair<Iterator, bool> insert(T& value);
-	pair<Iterator, bool> insert(RBNode<T> *node);
+	pair<Iterator, bool> insert(const T& value);
+	pair<Iterator, bool> insert(const RBNode<T> *node);
 #else
-	Iterator insert(T& value);
+	Iterator insert(const T& value);
 	Iterator insert(RBNode<T> *node);
 #endif
 
@@ -82,10 +82,15 @@ RBTree<T>::RBTree(RBNode<T> node)
 // operations on trees
 
 //Insertion
+/*
+ * tree_insert: inserts an RBNode into an RBTree, as though it were a regular tree.
+		This only modifies the tree_size_ value.
+ * @node:	pointer to the node that must be inserted
+ */
 template<typename T>
 void RBTree<T>::tree_insert(RBNode<T> *node)
 {
-	RBNode<T> *y = NIL;
+	RBNode<T> *y = nullptr;
 	RBNode<T> *x = root_;
 
 	while (x != NIL){
@@ -98,14 +103,16 @@ void RBTree<T>::tree_insert(RBNode<T> *node)
 	}
 	node->parent_ = y;
 
-	if(y == NIL)
+	if(y == nullptr)
 		root_ = node;
 
-	if(node->value_ < y->value_)
-		y->left_ = node;
-	else
-		y-> right_ = node;
-
+	else {
+		if(node->value_ < y->value_)
+			y->left_ = node;
+		else
+			y-> right_ = node;
+	}
+	++tree_size_;
 }
 
 #ifdef RBT_UNIQUE
@@ -130,7 +137,65 @@ pair<typename RBTree<T>::Iterator, bool> RBTree<T>::insert(RBTNode<T> *node)
 
 
 #else
+template<typename T>
+typename RBTree<T>::Iterator RBTree<T>::insert(const T& value)
+{
+	// TODO: is this actually required? need to test things more first.
+}
 
+template<typename T>
+typename RBTree<T>::Iterator RBTree<T>::insert(RBNode<T> *node)
+{
+	RBNode<T> *uncle;
+
+	tree_insert(node);
+
+	while((node != root_) && (node->parent_->color_ == RED)) {
+
+		if(node->parent_ == node->parent_->parent_->left_) {
+
+			uncle = node->parent_->parent_->right_;
+
+			if(uncle->color_ == RED) {
+				node->parent_->color_ = BLACK;
+				uncle->color_ = BLACK;
+				node->parent_->parent_->color_ = RED;
+				node = node->parent_->parent_;
+			}
+			else {
+				if (node = node->parent_->right_) {
+					node = node->parent_;
+					rotate_left(node);
+				}
+				node->parent_->color_ = BLACK;
+				node->parent_->parent_->color_ = RED;
+				rotate_right(node->parent_->parent_);
+			}
+		}
+		else {
+
+			uncle = node->parent_->parent_->left_;
+
+			if(uncle->color_ == RED) {
+				node->parent_->color_ = BLACK;
+				uncle->color_ = BLACK;
+				node->parent_->parent_->color_ = RED;
+				node = node->parent_->parent_;
+			}
+			else {
+				if (node = node->parent_->left_) {
+					node = node->parent_;
+					rotate_right(node);
+				}
+				node->parent_->color_ = BLACK;
+				node->parent_->parent_->color_ = RED;
+				rotate_left(node->parent_->parent_);
+			}
+		}
+	}
+	root_->color_ = BLACK;
+	return nullptr;
+}
 
 #endif
 

src/client.cpp
@@ -7,56 +7,72 @@ int main(void)
 {
 	RBTree<int> *tree = new RBTree<int>();
 
-	// same example as CLRS
-	RBNode<int> *lll = new RBNode<int>(2);
-	RBNode<int> *lr = new RBNode<int>(6);
-	RBNode<int> *rl = new RBNode<int>(9);
-	RBNode<int> *rrll = new RBNode<int>(12);
-	RBNode<int> *rrlr = new RBNode<int>(17);
-	RBNode<int> *rrrrl = new RBNode<int>(20);
+	#if 0
+		RBNode<int> *lll = new RBNode<int>(2);
+		RBNode<int> *lr = new RBNode<int>(6);
+		RBNode<int> *rl = new RBNode<int>(9);
+		RBNode<int> *rrll = new RBNode<int>(12);
+		RBNode<int> *rrlr = new RBNode<int>(17);
+		RBNode<int> *rrrrl = new RBNode<int>(20);
 
-	RBNode<int> *rrrr = new RBNode<int>(22, nullptr, rrrrl);
+		RBNode<int> *rrrr = new RBNode<int>(22, nullptr, rrrrl);
 
 
-	RBNode<int> *rrl = new RBNode<int>(14, nullptr, rrll, rrlr);
-	RBNode<int> *rrr = new RBNode<int>(19, nullptr, nullptr, rrrr);
+		RBNode<int> *rrl = new RBNode<int>(14, nullptr, rrll, rrlr);
+		RBNode<int> *rrr = new RBNode<int>(19, nullptr, nullptr, rrrr);
 
-	RBNode<int> *ll = new RBNode<int>(3, nullptr, lll, nullptr);
-	RBNode<int> *rr = new RBNode<int>(18, nullptr, rrl, rrr);
+		RBNode<int> *ll = new RBNode<int>(3, nullptr, lll, nullptr);
+		RBNode<int> *rr = new RBNode<int>(18, nullptr, rrl, rrr);
 
-	RBNode<int> *l = new RBNode<int>(4, nullptr, ll, lr);
-	RBNode<int> *r = new RBNode<int>(11, nullptr, rl, rr);
+		RBNode<int> *l = new RBNode<int>(4, nullptr, ll, lr);
+		RBNode<int> *r = new RBNode<int>(11, nullptr, rl, rr);
 
-	RBNode<int> *rootnode = new RBNode<int>(7, nullptr, l, r);
+		RBNode<int> *rootnode = new RBNode<int>(7, nullptr, l, r);
 
-	l->parent_ = rootnode;
-	r->parent_ = rootnode;
+		l->parent_ = rootnode;
+		r->parent_ = rootnode;
 
-	ll->parent_ = l;
-	rr->parent_ = r;
+		ll->parent_ = l;
+		rr->parent_ = r;
 
-	rrl->parent_ = rr;
-	rrr->parent_ = rr;
+		rrl->parent_ = rr;
+		rrr->parent_ = rr;
 
-	rrrr->parent_ = rrr;
-	lll->parent_ = ll;
-	lr->parent_ = l;
-	rl->parent_ = r;
-	rrll->parent_ = rrl;
-	rrlr->parent_ = rrl;
-	rrrrl->parent_ = rrrr;
+		rrrr->parent_ = rrr;
+		lll->parent_ = ll;
+		lr->parent_ = l;
+		rl->parent_ = r;
+		rrll->parent_ = rrl;
+		rrlr->parent_ = rrl;
+		rrrrl->parent_ = rrrr;
 
-	tree->root_ = rootnode;
+		tree->root_ = rootnode;
+	#endif
 
+	#if 1
+	// 11, 2, 14, 1, 15, 7, 5, 8, 4
+	RBNode<int> *roots = new RBNode<int>(11);
+	RBNode<int> *l = new RBNode<int>(2);
+	RBNode<int> *ll = new RBNode<int>(1);
+	RBNode<int> *lr = new RBNode<int>(7);
+	RBNode<int> *lrl = new RBNode<int>(5);
+	RBNode<int> *lrr = new RBNode<int>(8);
+	RBNode<int> *r = new RBNode<int>(14);
+	RBNode<int> *rr = new RBNode<int>(15);
+	RBNode<int> *newNode = new RBNode<int>(4);
+
+	tree->insert(roots);
+	tree->insert(l);
+	tree->insert(r);
+	tree->insert(ll);
+	tree->insert(lr);
+	tree->insert(rr);
+	tree->insert(lrl);
+	tree->insert(lrr);
+	tree->insert(newNode);
 	cout << *tree;
-	// tree->rotate_left(r);
+	#endif
 
-	RBNode<int> *insnode = new RBNode<int>(5);
-	tree->tree_insert(insnode);
-	cout << *tree;
-	// tree->print_inorder();
-
-	cout << tree->begin();
 
 	return 0;
 }

commit b5603f6d5d5eb439fae6f337c42a59c5ff5e4bd4

rb_tree.h: fix insert()
The insert function seems to give segmentation faults everytime a skewed
tree is created (e.g., if nodes were inserted in the order - 11, 4, 2).

Turns out, these issues were majorly being created since leaf nodes would
point to nullptr instead of NIL(and even if they did point to NIL, NIL
was a nullptr, so there was no real effect).
Making NIL actually a node (albeit an empty one), and rewriting the insert
function appropriately fixes this issue.

Modify the RBNode constructors, so that default constructor is in play,
and if not, atleast the value of the node has to be passed to the ctor.

Remove tree_insert(), and rewrite insert() in line with the actually
correct, true CLRS explanation. insert() now uses the helper function
insert_fixup() instead.

Fixes: 29c4c56 ("RBTree: define the basic insert() function")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_node.h
@@ -13,9 +13,9 @@ public:
 	RBNode *right_;
 	Color color_;
 
-	// RBNode() = default;
+	RBNode() = default;
 
-	RBNode(const T& value = 0, RBNode<T> *parent = nullptr, RBNode<T> *left= nullptr, RBNode<T> *right= nullptr, Color color = RED)
+	RBNode(const T& value, RBNode<T> *parent = nullptr, RBNode<T> *left= nullptr, RBNode<T> *right= nullptr, Color color = RED)
 	: value_(value), left_(left), right_(right), parent_(parent), color_(color)
 	{}
 

include/rb_tree.h
@@ -41,6 +41,7 @@ public:
 #else
 	Iterator insert(const T& value);
 	Iterator insert(RBNode<T> *node);
+	void insert_fixup(RBNode<T> *node);
 #endif
 
 	bool delete_node(const T& value);
@@ -68,13 +69,13 @@ private:
 
 // constructors
 template <typename T>
-RBNode<T> *RBTree<T>::NIL = nullptr;
+RBNode<T> *RBTree<T>::NIL = new RBNode<T>();
 
 // constructors
 template<typename T>
 RBTree<T>::RBTree()
 {
-	root_ = nullptr;
+	root_ = NIL;
 	tree_size_ = 0;
 }
 
@@ -91,8 +92,8 @@ template<typename T>
 RBTree<T>::RBTree(const RBTree<T> &rhs)
 : tree_size_(rhs.tree_size_)
 {
-	if(rhs.root_ == nullptr) {
-		root_ = nullptr;
+	if(rhs.root_ == NIL) {
+		root_ = NIL;
 		return;
 	}
 
@@ -103,8 +104,8 @@ template<typename T>
 RBNode<T>* RBTree<T>::copy_tree(RBNode<T> *root)
 {
 	RBNode<T> *new_left, *new_right, *new_node;
-	if(root == nullptr)
-		return nullptr;
+	if(root == NIL)
+		return NIL;
 
 	new_left = copy_tree(root->left_);
 	new_right = copy_tree(root->right_);
@@ -124,40 +125,6 @@ RBNode<T>* RBTree<T>::copy_tree(RBNode<T> *root)
 }
 
 // operations on trees
-
-//Insertion
-/*
- * tree_insert: inserts an RBNode into an RBTree, as though it were a regular tree.
-		This only modifies the tree_size_ value.
- * @node:	pointer to the node that must be inserted
- */
-template<typename T>
-void RBTree<T>::tree_insert(RBNode<T> *node)
-{
-	RBNode<T> *y = nullptr;
-	RBNode<T> *x = root_;
-
-	while (x != NIL){
-		y = x;
-
-		if(node->value_ < x->value_)
-			x = x->left_;
-		else
-			x = x->right_;
-	}
-	node->parent_ = y;
-
-	if(y == nullptr)
-		root_ = node;
-
-	else {
-		if(node->value_ < y->value_)
-			y->left_ = node;
-		else
-			y-> right_ = node;
-	}
-	++tree_size_;
-}
 
 #ifdef RBT_UNIQUE
 template<typename T>
@@ -187,58 +154,98 @@ typename RBTree<T>::Iterator RBTree<T>::insert(const T& value)
 	// TODO: is this actually required? need to test things more first.
 }
 
+
 template<typename T>
-typename RBTree<T>::Iterator RBTree<T>::insert(RBNode<T> *node)
+void RBTree<T>::insert_fixup(RBNode<T> *node)
 {
-	RBNode<T> *uncle;
+	RBNode<T> *parent = node->parent_;
+	while(parent && parent->color_ == RED) {
+		parent = node->parent_;
+		RBNode<T> *grandparent = parent->parent_;
 
-	tree_insert(node);
-	RBTree<T>::Iterator it = Iterator(node);
-
-	while((node != root_) && (node->parent_->color_ == RED)) {
-
-		if(node->parent_ == node->parent_->parent_->left_) {
-
-			uncle = node->parent_->parent_->right_;
+		if(parent == grandparent->left_) {
+			RBNode<T> *uncle = grandparent->right_;
 
 			if(uncle->color_ == RED) {
-				node->parent_->color_ = BLACK;
+				parent->color_ = BLACK;
 				uncle->color_ = BLACK;
-				node->parent_->parent_->color_ = RED;
-				node = node->parent_->parent_;
+				grandparent->color_ = RED;
+				node = grandparent;
 			}
 			else {
-				if (node = node->parent_->right_) {
-					node = node->parent_;
+				if(node == parent ->right_) {
+					node = parent;
 					rotate_left(node);
 				}
-				node->parent_->color_ = BLACK;
-				node->parent_->parent_->color_ = RED;
-				rotate_right(node->parent_->parent_);
+
+				parent->color_ = BLACK;
+				grandparent->color_ = RED;
+				rotate_right(grandparent);
+				
 			}
 		}
 		else {
-
-			uncle = node->parent_->parent_->left_;
+			RBNode<T> *uncle = grandparent->left_;
 
 			if(uncle->color_ == RED) {
-				node->parent_->color_ = BLACK;
+				parent->color_ = BLACK;
 				uncle->color_ = BLACK;
-				node->parent_->parent_->color_ = RED;
-				node = node->parent_->parent_;
+				grandparent->color_ = RED;
+				node = grandparent;
 			}
 			else {
-				if (node = node->parent_->left_) {
-					node = node->parent_;
+				if(node == parent->left_) {
+					node= parent;
 					rotate_right(node);
 				}
-				node->parent_->color_ = BLACK;
-				node->parent_->parent_->color_ = RED;
-				rotate_left(node->parent_->parent_);
+
+				parent->color_ = BLACK;
+				grandparent->color_ = RED;
+				rotate_left(grandparent);
+				
 			}
 		}
 	}
 	root_->color_ = BLACK;
+}
+
+
+
+template<typename T>
+typename RBTree<T>::Iterator RBTree<T>::insert(RBNode<T> *node)
+{
+	RBTree<T>::Iterator it = Iterator(node);
+	RBNode<T> *parent = NIL;
+	RBNode<T> *temp = root_;
+
+	while(temp != NIL) {
+		parent = temp;
+
+		if(node->value_ < temp->value_)
+			temp = temp->left_;
+
+		else
+			temp = temp->right_;
+
+	}
+
+	node->parent_ = parent;
+
+	if(parent == NIL)
+		root_ = node;
+
+	else if(node->value_ < parent->value_)
+		parent->left_ = node;
+
+	else
+		parent->right_ = node;
+
+	node->left_ = NIL;
+	node->right_ = NIL;
+	node-> color_ = RED;
+	++tree_size_;
+
+	insert_fixup(node);
 	return it;
 }
 

include/utils.h
@@ -9,7 +9,7 @@
 template<typename T>
 void print_tree(std::ostream& os, const std::string& prefix, const RBNode<T> *node, bool is_left)
 {
-	if(node == nullptr)
+	if(node -> left_ == nullptr && node->right_ == nullptr)
 		return;
 
 	os << prefix;

src/client.cpp
@@ -52,12 +52,12 @@ int main(void)
 	#if 1 // Create a tree using Insert
 		// 11, 2, 14, 1, 15, 7, 5, 8, 4
 		RBNode<int> *roots = new RBNode<int>(11);
-		RBNode<int> *l = new RBNode<int>(2);
+		RBNode<int> *l = new RBNode<int>(3);
 		RBNode<int> *ll = new RBNode<int>(1);
 		RBNode<int> *lr = new RBNode<int>(7);
 		RBNode<int> *lrl = new RBNode<int>(5);
 		RBNode<int> *lrr = new RBNode<int>(8);
-		RBNode<int> *r = new RBNode<int>(14);
+		RBNode<int> *r = new RBNode<int>(1);
 		RBNode<int> *rr = new RBNode<int>(15);
 		RBNode<int> *newNode = new RBNode<int>(4);
 
@@ -72,9 +72,12 @@ int main(void)
 		RBTree<int>::Iterator it_lrr = tree->insert(lrr);
 		RBTree<int>::Iterator it_new = tree->insert(newNode);
 		RBTree<int>::Iterator it_1 = tree->begin();
-
+		cout << *tree << tree;
+		RBTree<int> newtree = RBTree<int>(*tree);
+		cout << newtree << &newtree;
 		#if 0 // Test for level-order successors
-		
+
+
 		cout << *tree;
 		cout << "Levelorder successors!\n";
 		cout << it_lr;
@@ -127,7 +130,7 @@ int main(void)
 
 		#endif
 
-		#if 1 // Test for post-order successors
+		#if 0 // Test for post-order successors
 		tree->print_postorder();
 		cout << "Post-order successors!\n";
 		cout << it_lr;