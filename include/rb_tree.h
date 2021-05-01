thazhemadam/generic-red-black-trees

commit bf174ce560024136a6bf91b90c4cdfb2a672c3fd

rb_tree.h: add new constructor to initialize root node
add new constructor, which takes an RBNode as parameter and initializes
it to be the root node of the tree.

NOTE: This is pending testing. Can be tested only after the copy
constructor for RBNode is defined.

Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_tree.h
@@ -17,7 +17,8 @@ public:
 
 // special functions
 	// constructor
-	RBTree();
+	RBTree();	// empty tree
+	RBTree(RBNode<T> node);	// tree with a root node
 
 	// operator function
 	template<typename O>
@@ -29,7 +30,10 @@ public:
 	{
 		return Iterator(root_);
 	}
-	Iterator end();
+	Iterator end()
+	{
+		return NIL;
+	}
 
 // operations on tree
 #ifdef RBT_UNIQUE	//TODO
@@ -70,6 +74,11 @@ RBTree<T>::RBTree()
 	tree_size_ = 0;
 }
 
+// TODO - TEST THIS. Can be done only after copy constructor is created for RBNode.
+template<typename T>
+RBTree<T>::RBTree(RBNode<T> node)
+: root_(new RBNode<T>(node)), tree_size_(1)
+{}
 // operations on trees
 
 //Insertion

commit fe6694fda8af3b029b4e48a39584174535e20c5a

rb_tree.h: fix constructor that initializes root node for RBTree
Fixes: bf174ce ("rb_tree.h: add new constructor to initialize root node")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_tree.h
@@ -82,7 +82,9 @@ RBTree<T>::RBTree()
 template<typename T>
 RBTree<T>::RBTree(RBNode<T> node)
 : root_(new RBNode<T>(node)), tree_size_(1)
-{}
+{
+	root_->color_ = BLACK;
+}
 
 // copy ctor
 template<typename T>