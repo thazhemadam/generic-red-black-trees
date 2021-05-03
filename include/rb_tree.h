thazhemadam/generic-red-black-trees

commit e6f085b1b8a0da1e94651667ff7f7cdab4d74d35

rb_tree.h: add condition in delete_fixup()
In the case where the RBTree has only 3 nodes left, when delete_fixup()
is called, it will result in a segfault, as the nodes NIL
refers to (which are nullptrs) are accessed.

Fix this issue by adding a clause in the while loop that checks if the
node is NIL or not before proceeding.

Fixes: eb24260 ("rb_tree.h: fix delete - deletion of a single node")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_tree.h
@@ -416,7 +416,7 @@ template<typename T,typename Compare>
 void RBTree<T, Compare>::delete_fixup(RBNode<T> *x)
 {
 	RBNode<T>* w;
-	while(x != root_ && x->color_ == BLACK) {
+	while(x != root_ && x->color_ == BLACK && x != NIL) {
 		if(x = x->parent_->left_) {
 			w = x->parent_->right_;
 			if(w->color_ == RED) {

commit cbd2302410ea6e94b35b51e1c0013295d7862977

rb_tree.h: handle NIL nodes correctly in delete_fixup()
Commit e6f085b handled NIL nodes to prevent segfaults. While this prevented
segfaults, it also messed up the coloring of the tree, which was also
supposed to be fixed up. Further, the wrong node was being checked for
NIL, which just made things worse.
There was also an assignment being done incorrectly instead of a
comparison within the initial if condition in the while loop.

Update the check for NIL node better within the loop itself by
identifying the actual node which shouldn't be NIL, and enforcing a
check for the same. If the node ('w') is found to be NIL, then break out
of the while loop within delete_fixup().

Fixes: e6f085b ("rb_tree.h: add condition in delete_fixup()")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_tree.h
@@ -416,15 +416,21 @@ template<typename T,typename Compare>
 void RBTree<T, Compare>::delete_fixup(RBNode<T> *x)
 {
 	RBNode<T>* w;
-	while(x != root_ && x->color_ == BLACK && x != NIL) {
-		if(x = x->parent_->left_) {
+
+	while(x != root_ && x->color_ == BLACK) {
+
+		if(x == x->parent_->left_) {
 			w = x->parent_->right_;
+
 			if(w->color_ == RED) {
 				w->color_ = BLACK;
 				x->parent_->color_ = RED;
 				rotate_left(x->parent_);
 				w = x->parent_->right_;
 			}
+
+			if(!w->left_ && !w->right_) 	// w is a NIL node
+				break;
 
 			if(w->left_->color_ == BLACK && w->right_->color_ == BLACK) {
 				w->color_ = RED;
@@ -454,6 +460,9 @@ void RBTree<T, Compare>::delete_fixup(RBNode<T> *x)
 				rotate_right(x->parent_);
 				w = x->parent_->left_;
 			}
+
+			if(!w->left_ && !w->right_)	// w is a NIL node
+				break;
 
 			if(w->right_->color_ == BLACK && w->left_->color_ == BLACK) {
 				w->color_ = RED;