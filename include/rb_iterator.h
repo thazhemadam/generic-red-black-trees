5d0dddcbf62a3306584c6a9687d4756359674f12
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-05 04:48:04 +0530

    rb_iterator.h: add support for decrement operations
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- include/rb_iterator.h
+++ include/rb_iterator.h
@@ -15,12 +15,21 @@ public:
 	Iterator() = default;
 	Iterator(RBNode<T> *p_it) : p_it_(p_it) { }
 
-	// Note: incrementing follows standard pre-order traversal
-	Iterator& operator++();		// pre-increment
-	Iterator operator++(int);	// post-increment
 	RBNode<T>& operator*();		// dereference
-	bool operator==(const Iterator& rhs) const;	// equality
-	bool operator!=(const Iterator& rhs) const;	// inequality
+
+	// increment and decrement
+	Iterator& operator++();
+	Iterator operator++(int);
+	Iterator& operator--();
+	Iterator operator--(int);
+	// comparison
+	bool operator==(const Iterator& rhs) const;
+	bool operator!=(const Iterator& rhs) const;
+        bool operator>(const Iterator &) const;
+	bool operator<(const Iterator &) const;
+        bool operator<=(const Iterator &) const;
+        bool operator>=(const Iterator &) const;
+
 	void display(std::ostream& os = std::cout) const;
 
 	friend ostream& operator<<(ostream& os, const Iterator& iterator)
@@ -41,21 +50,6 @@ public:
 
 
 template<typename T, typename Compare>
-typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator++() // pre-increment
-{
-	p_it_ = inorder_successor();
-	return *this;
-}
-
-template<typename T, typename Compare>
-typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::Iterator::operator++(int) // post-increment
-{
-	Iterator temp(*this);
-	p_it_ = temp.p_it_->inorder_successor();
-	return temp;
-}
-
-template<typename T, typename Compare>
 RBNode<T>& RBTree<T, Compare>::Iterator::operator*()
 {
 	return *p_it_;
@@ -68,11 +62,13 @@ bool RBTree<T, Compare>::Iterator::operator==(const Iterator& rhs) const
 	return p_it_ == rhs.p_it_;
 }
 
+
 template<typename T, typename Compare>
 bool RBTree<T, Compare>::Iterator::operator!=(const Iterator& rhs) const
 {
 	return !(*this == rhs);
 }
+
 
 template<typename T, typename Compare>
 void RBTree<T, Compare>::Iterator::display(std::ostream& os) const
@@ -84,6 +80,41 @@ void RBTree<T, Compare>::Iterator::display(std::ostream& os) const
 	os << p_it_->value_ << "\t" << (p_it_->color_ == 0 ? "black" : "red")  << "\n";
 }
 
+
+
+
+template<typename T, typename Compare>
+typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator++() // pre-increment
+{
+	p_it_ = inorder_successor();
+	return *this;
+}
+
+
+template<typename T, typename Compare>
+typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::Iterator::operator++(int) // post-increment
+{
+	Iterator temp(*this);
+	p_it_ = temp.p_it_->inorder_successor();
+	return temp;
+}
+
+
+template<typename T, typename Compare>
+typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator--() // pre-increment
+{
+	p_it_ = inorder_predecessor();
+	return *this;
+}
+
+
+template<typename T, typename Compare>
+typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::Iterator::operator--(int) // post-increment
+{
+	Iterator temp(*this);
+	p_it_ = temp.p_it_->inorder_predecessor();
+	return temp;
+}
 
 
 template<typename T, typename Compare>
@@ -158,6 +189,7 @@ RBNode<T>* RBTree<T, Compare>::Iterator::levelorder_successor(const RBTree<T, Co
 	return q.front();
 }
 
+
 template<typename T, typename Compare>
 RBNode<T>* RBTree<T, Compare>::Iterator::preorder_predecessor(RBNode<T> *root_,const RBNode<T> *node)
 {
@@ -205,6 +237,7 @@ RBNode<T>* RBTree<T, Compare>::Iterator::preorder_successor()
 	// undefined case.
 	return nullptr;
 }
+
 
 template<typename T, typename Compare>
 RBNode<T>* RBTree<T, Compare>::Iterator::postorder_predecessor( RBNode<T>* root, RBNode<T> *node)
