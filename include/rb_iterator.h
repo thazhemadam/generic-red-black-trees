thazhemadam/generic-red-black-trees

commit a13b1098ad2da93b4890cf03fa355248987a800e

NIL: fix memory leak caused by NIL
NIL is a static pointer that points to dynamically allocated memory.
So, if left as-is, it leads to a memory leak. Converting NIL to a smart
pointer - unique_ptr, fixes this memory leak.

Note:
An atexit() function could be declared in the constructor too as an
alternative. However, this is less favorable, since the atexit()
function will get registered multiple times needlessly.

Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_iterator.h
@@ -155,14 +155,14 @@ RBNode<T>* RBTree<T, Compare>::Iterator::levelorder_successor(const RBTree<T, Co
 {
 	RBNode<T> *root = tree->root_;
 	RBNode<T> *node = p_it_;
-	if(root == NIL)
+	if(root == NIL.get())
 		return nullptr;
 
 	if(root == node) {
-		if(root->left_ != NIL)
+		if(root->left_ != NIL.get())
 			return root->left_;
 
-		else if(root->right_ != NIL)
+		else if(root->right_ != NIL.get())
 			return root->left_;
 
 		else
@@ -176,10 +176,10 @@ RBNode<T>* RBTree<T, Compare>::Iterator::levelorder_successor(const RBTree<T, Co
 		RBNode<T> *temp = q.front();
 		q.pop();
 
-		if(temp -> left_ != NIL)
+		if(temp -> left_ != NIL.get())
 			q.push(temp->left_);
 
-		if(temp -> right_ != NIL)
+		if(temp -> right_ != NIL.get())
 			q.push(temp->right_);
 
 		if(temp == node)
@@ -211,23 +211,23 @@ template<typename T, typename Compare>
 RBNode<T>* RBTree<T, Compare>::Iterator::preorder_successor()
 {
 	RBNode<T> *node = p_it_;
-	if(node == NIL)
+	if(node == NIL.get())
 		return nullptr;
 
 	if(node->parent_ == nullptr)	// has no children, and is root.
 		return nullptr;
 
-	if (node->left_ && node->left_ !=  NIL)	// has left child.
+	if (node->left_ && node->left_ !=  NIL.get())	// has left child.
 		return node->left_;
 
-	else if (node->right_ && node->right_ != NIL)	// has right child.
+	else if (node->right_ && node->right_ != NIL.get())	// has right child.
 		return node->right_;
 
 	RBNode<T> *parent = node->parent_;
 
 	// leaf node. has an ancestor/is a node which is a left child + has a sibling.
 	while(parent != nullptr) {
-		if(parent->left_ == node && parent->right_ != NIL)
+		if(parent->left_ == node && parent->right_ != NIL.get())
 			return parent->right_;
 
 		node = parent;
@@ -270,28 +270,28 @@ template<typename T, typename Compare>
 RBNode<T>* RBTree<T, Compare>::Iterator::postorder_successor()
 {
 	RBNode<T> *node = p_it_;
-	if(node == NIL)
+	if(node == NIL.get())
 		return nullptr;
 
-	if(node->parent_ == NIL)	// node is root. undefined case.
+	if(node->parent_ == NIL.get())	// node is root. undefined case.
 		return nullptr;
 
 	RBNode<T> *parent = node->parent_;
 
 	// is the right child, or is a left child with no siblings
-	if(node == parent->right_ || parent->right_ == NIL)
+	if(node == parent->right_ || parent->right_ == NIL.get())
 		return parent;
 
 	// is the left child, with a right sibling
 	RBNode<T> *temp = parent->right_;
 	// until it is the next a leaf node. left-most leafnodes are given priority.
-	while(temp->left_ != NIL || temp->right_ != NIL) {
-		if(temp->left_ != NIL){
+	while(temp->left_ != NIL.get() || temp->right_ != NIL.get()) {
+		if(temp->left_ != NIL.get()){
 			temp = temp->left_;
 			continue;
 		}
 
-		else if(temp->right_ != NIL){
+		else if(temp->right_ != NIL.get()){
 			temp = temp->right_;
 			continue;
 		}
@@ -307,12 +307,12 @@ RBNode<T>* RBTree<T, Compare>::Iterator::inorder_successor()
 {
 	RBNode<T>* node = p_it_;
 
-	if(node == NIL)
+	if(node == NIL.get())
 		return nullptr;
 
-	if(node->right_ != NIL) {	// has a right child.
+	if(node->right_ != NIL.get()) {	// has a right child.
 		RBNode<T> *temp = node->right_;
-		while(temp->left_ != NIL)
+		while(temp->left_ != NIL.get())
 			temp = temp->left_;
 		
 		return temp;
@@ -321,12 +321,12 @@ RBNode<T>* RBTree<T, Compare>::Iterator::inorder_successor()
 	RBNode<T>* parent = node->parent_;
 
 	// no right child. successor is the node whose left child
-	while (parent != NIL && parent->left_ != node) {
+	while (parent != NIL.get() && parent->left_ != node) {
 		node = node->parent_;
 		parent = node->parent_;
 	}
 
-	return parent == NIL ? nullptr : parent;
+	return parent == NIL.get() ? nullptr : parent;
 }
 
 template<typename T, typename Compare>

include/rb_tree.h
@@ -3,6 +3,7 @@
 
 #include "rb_node.h"
 #include "utils.h"
+#include <memory>
 using namespace std;
 
 template <typename T = int, typename Compare = std::less<T>>
@@ -26,7 +27,7 @@ private:
 	void remove_fixup(RBNode<T> *x);
 
 public:
-	static RBNode<T> *NIL;
+	static unique_ptr<RBNode<T>> NIL;
 	int tree_size_;
 
 // special functions
@@ -84,14 +85,14 @@ public:
 
 // constructors
 template <typename T, typename Compare>
-RBNode<T> *RBTree<T, Compare>::NIL = new RBNode<T>();
+unique_ptr<RBNode<T>> RBTree<T, Compare>::NIL = unique_ptr<RBNode<T>>(new RBNode<T>());
 
 
 // constructors
 template<typename T, typename Compare>
 RBTree<T, Compare>::RBTree()
 {
-	root_ = NIL;
+	root_ = NIL.get();
 	tree_size_ = 0;
 }
 
@@ -117,8 +118,8 @@ template<typename T, typename Compare>
 RBTree<T, Compare>::RBTree(const RBTree<T, Compare> &rhs)
 : tree_size_(rhs.tree_size_)
 {
-	if(rhs.root_ == NIL) {
-		root_ = NIL;
+	if(rhs.root_ == NIL.get()) {
+		root_ = NIL.get();
 		return;
 	}
 
@@ -130,8 +131,8 @@ template<typename T, typename Compare>
 RBNode<T>* RBTree<T, Compare>::copy_tree(RBNode<T> *root)
 {
 	RBNode<T> *new_left, *new_right, *new_node;
-	if(root == NIL)
-		return NIL;
+	if(root == NIL.get())
+		return NIL.get();
 
 	new_left = copy_tree(root->left_);
 	new_right = copy_tree(root->right_);
@@ -156,17 +157,17 @@ void RBTree<T, Compare>::rotate_left(RBNode<T> *pivot)
 {
 	RBNode<T>* pivot_right = pivot->right_;
 
-	if(pivot_right == NIL)
+	if(pivot_right == NIL.get())
 		return;
 
 	pivot->right_= pivot_right->left_;
 
-	if(pivot_right->left_ != NIL)
+	if(pivot_right->left_ != NIL.get())
 		pivot_right->left_->parent_ = pivot;
 
 	pivot_right->parent_ = pivot->parent_;
 
-	if(pivot->parent_ == NIL)
+	if(pivot->parent_ == NIL.get())
 		root_ = pivot_right;
 
 	else if(pivot == pivot->parent_->left_)
@@ -185,17 +186,17 @@ void RBTree<T, Compare>::rotate_right(RBNode<T> *pivot)
 {
 	RBNode<T>* pivot_left = pivot->left_;
 
-	if(pivot_left == NIL)
+	if(pivot_left == NIL.get())
 		return;
 
 	pivot->left_= pivot_left->right_;
 
-	if(pivot_left->right_ != NIL)
+	if(pivot_left->right_ != NIL.get())
 		pivot_left->right_->parent_ = pivot;
 
 	pivot_left->parent_ = pivot->parent_;
 
-	if(pivot->parent_ == NIL)
+	if(pivot->parent_ == NIL.get())
 		root_ = pivot_left;
 
 	else if(pivot == pivot->parent_->right_)
@@ -223,10 +224,10 @@ template<typename T, typename Compare>
 typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::insert(RBNode<T> *node)
 {
 	RBTree<T, Compare>::Iterator it = Iterator(node);
-	RBNode<T> *parent = NIL;
+	RBNode<T> *parent = NIL.get();
 	RBNode<T> *temp = root_;
 
-	while(temp != NIL) {
+	while(temp != NIL.get()) {
 		parent = temp;
 
 		if(compare(node->value_ , temp->value_))
@@ -239,7 +240,7 @@ typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::insert(RBNode<T> *node
 
 	node->parent_ = parent;
 
-	if(parent == NIL)
+	if(parent == NIL.get())
 		root_ = node;
 
 	else if(compare(node->value_ , parent->value_))
@@ -248,8 +249,8 @@ typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::insert(RBNode<T> *node
 	else
 		parent->right_ = node;
 
-	node->left_ = NIL;
-	node->right_ = NIL;
+	node->left_ = NIL.get();
+	node->right_ = NIL.get();
 	node-> color_ = RED;
 
 	++tree_size_;
@@ -335,7 +336,7 @@ typename RBTree<T,Compare>::Iterator RBTree<T,Compare>::search(RBNode<T> *node) 
 
 	RBNode<T> *temp = root_;
 
-	while(temp != NIL) {
+	while(temp != NIL.get()) {
 		if(temp->value_ == node->value_)
 			return Iterator(temp);
 
@@ -353,7 +354,7 @@ typename RBTree<T,Compare>::Iterator RBTree<T,Compare>::search(RBNode<T> *node) 
 template<typename T,typename Compare>
 void RBTree<T, Compare>::rb_transplant(RBNode<T> *u,RBNode<T> *v)
 {
-	if(u->parent_ == NIL)
+	if(u->parent_ == NIL.get())
 		root_ = v;
 
 	else if(u == u->parent_->left_)
@@ -398,7 +399,7 @@ template<typename T,typename Compare>
 RBNode<T>* RBTree<T, Compare>::tree_minimum(RBNode<T> *root)
 {
 	RBNode<T> *temp = root;
-	while(temp -> left_ != NIL) {
+	while(temp -> left_ != NIL.get()) {
 		temp = temp->left_;
 	}
 	return temp;
@@ -487,12 +488,12 @@ void RBTree<T, Compare>::remove(RBNode<T> *node)
 	RBNode<T> *x;
 	RBNode<T> *y = node;
 	Color y_original_color = y->color_;
-	if(node->left_ == NIL) {
+	if(node->left_ == NIL.get()) {
 		x = node->right_;
 		rb_transplant(node, node->right_);
 	}
 
-	else if(node->right_ == NIL) {
+	else if(node->right_ == NIL.get()) {
 		x = node->left_;
 		rb_transplant(node, node->left_);
 	}

commit b683243023f5231ddadc60fb70745519d03c46a6

rb_iterator.h: update all instances of NIL to NIL.get()
While a13b109 updated most instances of NIL, some instances were left
unchanged.
Update all instances, so things compile correctly.

Fixes: a13b109 ("NIL: fix memory leak caused by NIL")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

include/rb_iterator.h
@@ -334,12 +334,12 @@ RBNode<T>* RBTree<T, Compare>::Iterator::inorder_predecessor()
 {
 	RBNode<T>* node = p_it_;
 
-	if(node == NIL)
+	if(node == NIL.get())
 		return nullptr;
 
-	if(node->left_ != NIL) {	// if it has a left child
+	if(node->left_ != NIL.get()) {	// if it has a left child
 		RBNode<T> *temp = node->left_;
-		while(temp->right_ != NIL)
+		while(temp->right_ != NIL.get())
 			temp = temp->right_;
 		
 		return temp;
@@ -348,12 +348,12 @@ RBNode<T>* RBTree<T, Compare>::Iterator::inorder_predecessor()
 	// finding the ancestor whose right descendant is node
 	RBNode<T>* parent = node->parent_;
 
-	while (parent != NIL && parent->right_ != node) {
+	while (parent != NIL.get() && parent->right_ != node) {
 		node = node->parent_;
 		parent = node->parent_;
 	}
 
-	return parent == NIL ? nullptr : parent;
+	return parent == NIL.get() ? nullptr : parent;
 }
 
 