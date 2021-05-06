c77b3f99de7e69a09b4bba510215a3223f114707
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-06 10:41:11 +0530

    rb_iterator.h: ++ and -- return NIL and actively prevent OOB-accesses
    Instead of having the increment and decrement operations allow an
    iterator to point to nullptr, point to NIL instead (NIL has become the
    de-facto stand-in for nullptr in this implementation anyways).
    If either of these operations are applied on an iterator that is already
    pointing to NIL, do nothing, and simply return the iterator as-is, still
    pointing to NIL.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- include/rb_iterator.h
+++ include/rb_iterator.h
@@ -132,7 +132,14 @@ bool RBTree<T, Compare>::Iterator::hasprev()
 template<typename T, typename Compare>
 typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator++() // pre-increment
 {
+	if(p_it_ == NIL.get())
+		return *this;
+
 	p_it_ = inorder_successor();
+
+	if(p_it_ == nullptr)
+		p_it_ = NIL.get();
+ 
 	return *this;
 }
 
@@ -140,26 +147,50 @@ typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator++(
 template<typename T, typename Compare>
 typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::Iterator::operator++(int) // post-increment
 {
+	if(p_it_ == NIL.get())
+		return *this;
+
 	Iterator temp(*this);
+
 	p_it_ = temp.inorder_successor();
+
+	if(p_it_ == nullptr)
+		p_it_ = NIL.get();
+
 	return temp;
 }
 
 
 template<typename T, typename Compare>
-typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator--() // pre-increment
+typename RBTree<T, Compare>::Iterator& RBTree<T, Compare>::Iterator::operator--() // pre-decrement
 {
+	if(p_it_ == NIL.get())
+		return *this;
+
 	p_it_ = inorder_predecessor();
+
+	if(p_it_ == nullptr)
+		p_it_ = NIL.get();
+ 
 	return *this;
 }
 
 
 template<typename T, typename Compare>
-typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::Iterator::operator--(int) // post-increment
+typename RBTree<T, Compare>::Iterator RBTree<T, Compare>::Iterator::operator--(int) // post-decrement
 {
+	if(p_it_ == NIL.get())
+		return *this;
+
 	Iterator temp(*this);
+
 	p_it_ = temp.inorder_predecessor();
+
+	if(p_it_ == nullptr)
+		p_it_ = NIL.get();
+
 	return temp;
+
 }
 
 template<typename T, typename Compare>
